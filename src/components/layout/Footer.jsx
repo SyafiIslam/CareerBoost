42fa94bc07ac3311230d79f3cad4d9b72ae88b0e
BlueFZ <muhammadsyafi@student.ub.ac.id>
2023-03-02 21:58:09 +0700

    footer sementara

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='hidden xl:    flex flex-col text-white bg-primary400'>
+    <div className='hidden xl:flex flex-col text-white bg-primary400'>
       <div className='flex pt-8 gap-80'>
         <div className='flex flex-col gap-5 basis-1/3 justify-start items-center'>
           <h1 className='font-bold text-2xl'>
