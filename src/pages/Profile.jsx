3178f33bbb0b23a8900b71a5ff32dd705d9c2cba
BlueFZ <muhammadsyafi@student.ub.ac.id>
2023-03-09 16:54:00 +0700

    profile nav

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,24 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, Outlet } from "react-router-dom";
 import WebLayout from "../layout/WebLayout";
-import avatar2 from '../assets/profile/avatar2.svg'
+import avatar2 from "../assets/profile/avatar2.svg";
 
 const Profile = () => {
+  const [click, setClick] = useState("data");
   return (
     <WebLayout>
       <div className="flex flex-col xl:flex-row p-6 gap-8">
         <div className="flex flex-col basis-3/12">
           <div className="flex flex-col gap-8 bg-neutral-50 py-6 xl:py-10 px-8 rounded-xl shadow-xl mb-4 xl:mb-8">
-            <img className=" md:w-3/12 xl:w-5/12 mx-auto" src={avatar2} alt="" />
+            <img
+              className=" md:w-3/12 xl:w-5/12 mx-auto"
+              src={avatar2}
+              alt=""
+            />
             <div className="flex flex-col gap-4 justify-center items-start">
               <h1 className="font-bold h5">Pengaturan Akun</h1>
-              <Link to="data" className="h6 hover:font-bold hover:text-primary400 transition-all duration-300">
+              <Link
+                onClick={() => {setClick('data')}}
+                to="data"
+                className={`h6 ${
+                  click === "data" && "text-primary400 font-bold"
+                } hover:font-bold hover:text-primary400 transition-all duration-300`}
+              >
                 Data Pribadi
               </Link>
-              <Link to="pengalaman" className="h6 hover:font-bold hover:text-primary400 transition-all duration-300">
+              <Link
+                onClick={() => {
+                  setClick("pengalaman");
+                }}
+                to="pengalaman"
+                className={`h6 ${
+                  click === "pengalaman" && "text-primary400 font-bold"
+                } hover:font-bold hover:text-primary400 transition-all duration-300`}
+              >
                 Pengalaman
               </Link>
-              <Link to="langganan" className="h6 hover:font-bold hover:text-primary400 transition-all duration-300">
+              <Link
+                to="langganan"
+                className={`h6 ${
+                  click === "langganan" && "text-primary400 font-bold"
+                } hover:font-bold hover:text-primary400 transition-all duration-300`}
+                onClick={() => {
+                  setClick("langganan");
+                }}
+              >
                 Langganan
               </Link>
             </div>
