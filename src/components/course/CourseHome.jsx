9aabfbc6b88687d7619df3476b6e5aecbbf4c7fb
BlueFZ <muhammadsyafi@student.ub.ac.id>
2023-03-18 05:01:31 +0700

    fix: course

---
+++ src/components/course/CourseHome.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+import { ImSpinner2 } from "react-icons/im";
+import BaseURL from "../../api/BaseURL";
+import Card from "../card/CourseCard";
+
+const CourseHome = () => {
+  const [home, setHome] = useState([]);
+  const [load, setLoad] = useState(false);
+  const token = window.localStorage.getItem("token");
+
+  const getHome = async () => {
+    await BaseURL.get("/api/courseinfo/rekomendasi", {
+      headers: { Authorization: `Bearer ${token}` },
+    })
+      .then((res) => {
+        setHome (res.data.data);
+      })
+      .catch(() => {});
+  };
+
+  useEffect(() => {
+    setLoad(true);
+    getHome();
+    setLoad(false);
+  });
+
+  if (load)
+    return (
+      <div className="container-svg flex justify-center items-center h-screen">
+        <ImSpinner2 className="w-24 h-24 animate-spin text-primary400" />
+      </div>
+    );
+  return (
+    <div>
+      <div className="flex items-center justify-between mt-8 p-4 xl:px-20 md:p-10">
+        <h2 className="font-bold text-neutral-900 h3">Recommended</h2>
+        <p className="text-primary500 h4">See All</p>
+      </div>
+
+      <div className="flex justify-center gap-10 flex-wrap mt-8 p-4">
+        {home.map((list) => {
+          return <Card data={list} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default CourseHome;
--- src/pages/Course.jsx
+++ src/pages/Course.jsx
@@ -5,114 +5,47 @@ import icon from "../assets/course/search.svg";
 import Card from "../components/card/CourseCard";
 import BaseURL from "../api/BaseURL";
 import { ImSpinner2 } from "react-icons/im";
+import Rekomendasi from "../components/course/Rekomendasi";
+import CourseHome from "../components/course/CourseHome";
 
 const Course = () => {
-  const [rec, setRec] = useState([]);
-  const [home, setHome] = useState([]);
-  const [load, setLoad] = useState(false);
-  const token = window.localStorage.getItem("token");
-
-  const getRecCourse = async () => {
-    await BaseURL.get("/api/courseinfo/rekomendasi", {
-      headers: { Authorization: `Bearer ${token}` },
-    })
-      .then((res) => {
-        setRec(res.data.data);
-      })
-      .catch(() => {});
-  };
-
-  const getHomeCourse = async () => {
-    await BaseURL.get("/api/courseinfo?search=", {
-      headers: { Authorization: `Bearer ${token}` },
-    })
-      .then((res) => {
-        setHome(res.data.data);
-      })
-      .catch(() => {})
-      .finally(() => {setLoad(false)})
-      
-  };
-
-  useEffect(() => {
-    setLoad(true);
-    getRecCourse();
-    getHomeCourse();
-  }, [token]);
 
   return (
     <WebLayout>
-      {load ? (
-        <div className="container-svg flex justify-center items-center h-screen">
-          <ImSpinner2 className="w-24 h-24 animate-spin text-primary400" />
-        </div>
-      ) : (
-        <div>
-          <div className="mt-8 p-4 xl:px-16 flex flex-col xl:flex-row xl:items-center xl:justify-evenly xl:gap-28">
-            <div className="flex flex-col gap-5 xl:w-6/12">
-              <h1 className="h3 font-bold text-primary600 text-center xl:text-left">
-                Kursus Online Bersertifikat Disertai Mini Project untuk Menambah
-                Portofolio
-              </h1>
-              <p className="text-neutral-600 text-center xl:text-left h6">
-                Mini Project sesuai kursus online yang kamu ambil, jadikan
-                portofolio untuk melamar magang dan pekerjaan!
-              </p>
-            </div>
-
-            <div className="mt-8 xl:w-5/12 flex justify-center">
-              <img className="w-10/12" src={jumbo} alt="" />
-            </div>
-          </div>
-
-          <div className="flex justify-center flex-row-reverse mt-10">
-            <input
-              type="text"
-              placeholder="Search"
-              className="w-10/12 py-2 px-3 xl:py-6 xl:px-20 pl-12 border border-neutral-700 rounded-full focus:outline-none bg-neutral-100 xl:text-2xl xl:w-8/12 "
-            />
-            <img
-              src={icon}
-              className="w-6 -mr-10 xl:-mr-16 scale-75 xl:scale-100 relative"
-              alt=""
-            />
-          </div>
-
-          <div className="flex items-center justify-between mt-8 p-4 xl:px-20 md:p-10">
-            <h2 className="font-bold text-neutral-900 h3">Recommended</h2>
-            <p className="text-primary500 h4">See All</p>
-          </div>
-
-          <div className="flex justify-center gap-10 flex-wrap mt-8 p-4">
-            {rec.map((list) => {
-              return (
-                <Card
-                  data={list}
-                />
-              );
-            })}
-          </div>
-
-          <div className="flex items-center justify-between mt-8 p-4 xl:px-20 md:p-10">
-            <h2 className="font-bold text-neutral-900 h3">
-              Interest
-            </h2>
-            <p className="text-primary500 text-lg md:text-xl xl:text-3xl">
-              See All
+      <div>
+        <div className="mt-8 p-4 xl:px-16 flex flex-col xl:flex-row xl:items-center xl:justify-evenly xl:gap-28">
+          <div className="flex flex-col gap-5 xl:w-6/12">
+            <h1 className="h3 font-bold text-primary600 text-center xl:text-left">
+              Kursus Online Bersertifikat Disertai Mini Project untuk Menambah
+              Portofolio
+            </h1>
+            <p className="text-neutral-600 text-center xl:text-left h6">
+              Mini Project sesuai kursus online yang kamu ambil, jadikan
+              portofolio untuk melamar magang dan pekerjaan!
             </p>
           </div>
 
-          <div className="flex justify-center gap-10 flex-wrap mt-8 p-4">
-            {home.map((data) => {
-              return (
-                <Card
-                  data={data}
-                />
-              );
-            })}
+          <div className="mt-8 xl:w-5/12 flex justify-center">
+            <img className="w-10/12" src={jumbo} alt="" />
           </div>
         </div>
-      )}
+
+        <div className="flex justify-center flex-row-reverse mt-10">
+          <input
+            type="text"
+            placeholder="Search"
+            className="w-10/12 py-2 px-3 xl:py-6 xl:px-20 pl-12 border border-neutral-700 rounded-full focus:outline-none bg-neutral-100 xl:text-2xl xl:w-8/12 "
+          />
+          <img
+            src={icon}
+            className="w-6 -mr-10 xl:-mr-16 scale-75 xl:scale-100 relative"
+            alt=""
+          />
+        </div>
+
+        <Rekomendasi />
+        <CourseHome />
+      </div>
     </WebLayout>
   );
 };
